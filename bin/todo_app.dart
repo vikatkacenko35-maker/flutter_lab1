d0f69c233c27121395442276649cf0dcd79acdef
karasidnez <karasdnez@gmail.com>
2026-04-08 15:22:22 +0300

    add package ansicolor

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,6 +1,12 @@
 import 'dart:io';
 import 'package:todo_app/todo.dart';
 import 'package:todo_app/todo_repository.dart';
+import 'package:ansicolor/ansicolor.dart';
+
+final AnsiPen greenPen = AnsiPen()..green();
+final AnsiPen redPen = AnsiPen()..red();
+final AnsiPen bluePen = AnsiPen()..blue();
+final AnsiPen yellowPen = AnsiPen()..yellow();
 
 void main() {
   TodoRepository repo = TodoRepository();
@@ -23,7 +29,7 @@ void main() {
 }
 
 void printMenu() {
-  print("Консольное приложение TODO");
+  print(yellowPen("Консольное приложение TODO"));
   print("Команды:");
   print(" add <текст>   - добавить задачу");
   print("list           - показать список");
@@ -98,7 +104,7 @@ bool handleCommand(TodoRepository repo, String input) {
         print("Неизвестная команда");
     }
   } catch (e) {
-    print("Ошибка: $e");
+    print(redPen("Ошибка: $e"));
   }
   return false;
 }
